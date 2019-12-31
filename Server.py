8056a3ac28eafa4e8abf5824335935a0c07dee95
Devon Smith <whiterwing@gmail.com>
2019-12-31 15:25:22 -0600

    added a reference to the webpage I used for the network protocols.

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 '''
     This is the Server side of a chat application.
+    Based on the work from:
+        https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
 '''
 
 import socket
